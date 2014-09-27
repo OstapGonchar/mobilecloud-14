a97bccb89b103051383ce517b23c119dacfd293c
Ostap Gonchar <ostap.gonchar@gmail.com>
2014-09-27 14:05:32 +0300

    Added JPS Entity to Video.

--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -18,34 +18,42 @@
 
 package org.magnum.mobilecloud.video;
 
+import com.google.common.collect.Lists;
+import org.magnum.mobilecloud.video.client.VideoSvcApi;
+import org.magnum.mobilecloud.video.repository.Video;
+import org.magnum.mobilecloud.video.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class VideoController {
-	
-	/**
-	 * You will need to create one or more Spring controllers to fulfill the
-	 * requirements of the assignment. If you use this file, please rename it
-	 * to something other than "AnEmptyController"
-	 * 
-	 * 
-		 ________  ________  ________  ________          ___       ___  ___  ________  ___  __       
-		|\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \     
-		\ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_   
-		 \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \  
-		  \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \ 
-		   \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
-		    \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
-                                                                                                                                                                                                                                                                        
-	 * 
-	 */
-	
-	@RequestMapping(value="/go",method=RequestMethod.GET)
-	public @ResponseBody String goodLuck(){
-		return "Good Luck!";
-	}
-	
+    public static final int ZERO_LIKES = 0;
+    @Autowired
+    private VideoRepository videos;
+
+    @RequestMapping(value = "/go", method = RequestMethod.GET)
+    public @ResponseBody String goodLuck() {
+        return "Good Luck!";
+    }
+
+    @RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.POST)
+    public @ResponseBody Video addVideo(@RequestBody Video v) {
+        v.setLikes(ZERO_LIKES);
+        videos.save(v);
+        return v;
+    }
+
+    @RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.GET)
+    public @ResponseBody Collection<Video> getVideoList(){
+        return Lists.newArrayList(videos.findAll());
+    }
 }
